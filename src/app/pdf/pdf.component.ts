cf1ad833ab09d9db0ab4c69f78919bbcff24f004
Roshan-Madushanka <74401961+Roshan-Madushanka@users.noreply.github.com>
2020-11-17 15:23:15 +0530

    first commit

---
+++ src/app/pdf/pdf.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf',
+  template: `
+    <p>
+      pdf works!
+    </p>
+  `,
+  styles: []
+})
+export class PdfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
